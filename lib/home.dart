eda698ab2b7becef572181fcbaef290e06868e7a
vortech <vortech@hxpredatorinterpolationhxphyper>
2025-01-02 17:39:37 +0100

    Initial commit

--- lib/home.dart
+++ lib/home.dart
@@ -10,144 +10,100 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   bool isExpanded = false;
 
-  void toggleContainer() {
-    if (!isExpanded) {
-      // Only allow expansion when the container is not expanded
-      setState(() {
-        isExpanded = true;
-      });
-    }
+  void toggleExpand() {
+    setState(() {
+      isExpanded = !isExpanded;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: GestureDetector(
-          onTap: toggleContainer, // Detect tap to expand
-          child: AnimatedContainer(
-            width: isExpanded ? 320 : 250,
-            height: isExpanded ? 250 : 50,
-            duration: const Duration(milliseconds: 300),
-            curve: Curves.easeInOut,
-            decoration: BoxDecoration(
-              color: isExpanded ? Colors.white : Colors.red,
-              borderRadius: BorderRadius.circular(25),
-              boxShadow: isExpanded
-                  ? [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 10,
-                        spreadRadius: 1,
-                        offset: Offset(0, 5),
-                      )
-                    ]
-                  : [],
-            ),
-            child: isExpanded
-                ? SingleChildScrollView(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Icon(
-                                Icons.warning_amber_rounded,
-                                color: Colors.red,
-                                size: 40,
-                              ),
-                              GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    isExpanded = false;
-                                  });
-                                },
-                                child: Icon(
-                                  Icons.close,
-                                  color: Colors.grey,
-                                  size: 20,
-                                ),
-                              )
-                            ],
-                          ),
-                          const SizedBox(height: 10),
-                          const Text(
-                            'Are you sure?',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          const Text(
-                            'You might not have backed up your important data, and proceeding could lead to data loss. ',
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.black54,
-                            ),
-                          ),
-                          const SizedBox(height: 20),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    isExpanded = false;
-                                  });
-                                },
-                                child: Container(
-                                  height: 40,
-                                  width: 130,
-                                  decoration: BoxDecoration(
-                                    color: Colors.grey[300],
-                                    borderRadius: BorderRadius.circular(16),
-                                  ),
-                                  child: Center(
-                                      child: Text(
-                                    'Cancel',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.black),
-                                  )),
-                                ),
-                              ),
-                              Container(
-                                height: 40,
-                                width: 130,
-                                decoration: BoxDecoration(
-                                  color: Colors.red,
-                                  borderRadius: BorderRadius.circular(16),
-                                ),
-                                child: Center(
-                                    child: Text(
-                                  'Continue',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                )),
-                              ),
-                            ],
+      backgroundColor: const Color(0xFF27282E),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: toggleExpand,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 600),
+                curve: Curves.fastLinearToSlowEaseIn,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Stop Button
+                    AnimatedContainer(
+                      duration: const Duration(milliseconds: 600),
+                      curve: Curves.easeInOutCubic,
+                      height: 60,
+                      width: isExpanded ? 160 : 300,
+                      decoration: BoxDecoration(
+                        color: isExpanded
+                            ? Colors.red
+                            : const Color.fromARGB(255, 65, 67, 77),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Stop',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+
+                    // Spacing between buttons
+                    isExpanded
+                        ? SizedBox(
+                            width: 16,
                           )
+                        : SizedBox(
+                            width: 16,
+                          ),
+                    // Resume Button
+                    AnimatedContainer(
+                      duration: const Duration(milliseconds: 600),
+                      curve: Curves.easeInOutCubic,
+                      height: 60,
+                      width: isExpanded ? 160 : 0,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 65, 67, 77),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Stack(
+                        children: [
+                          TweenAnimationBuilder<double>(
+                            tween: Tween<double>(
+                              begin: isExpanded ? 160 : 50,
+                              end: isExpanded ? 50 : 160,
+                            ),
+                            duration: const Duration(milliseconds: 600),
+                            curve: Curves.easeInOutCubic,
+                            builder: (context, value, child) {
+                              return Positioned(
+                                left: value,
+                                top: 0,
+                                bottom: 0,
+                                child: Center(child: child),
+                              );
+                            },
+                            child: const Text(
+                              'Resume',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
                         ],
                       ),
                     ),
-                  )
-                : const Center(
-                    child: Text(
-                      'Continue',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-          ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
